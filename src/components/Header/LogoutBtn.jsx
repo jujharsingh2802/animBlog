193f08ab6eb997095ccd997747c3e97097154f8a
jujharsingh2802 <sjujhar532@gmail.com>
2024-05-05 02:02:49 +0530

    responsiveness inc

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -14,7 +14,7 @@ function LogoutBtn() {
 
 
   return (
-    <button className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+    <button className='inline-block px-4 sm:px-6 py-1 sm:py-2 duration-200 hover:bg-blue-100 rounded-full'
     onClick={logoutHandler}
     >LogOut</button>
   )
